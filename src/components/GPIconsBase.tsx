b6e7af953854e21079a1cdcb0f845d467ff308f5
andrew-oid <andrewoidamuda@gmail.com>
2022-05-25 12:20:08 +0300

    created icons base module

---
+++ src/components/GPIconsBase.tsx
@@ -0,0 +1,13 @@
+import add from '../resources/Images/add.svg'
+import arrow from '../resources/Images/arrow.svg'
+import attributes from '../resources/Images/attributes.svg'
+import brands from '../resources/Images/brands.svg'
+import categories from '../resources/Images/categories.svg'
+import orders from '../resources/Images/orders.svg'
+import special from '../resources/Images/special.svg'
+import variableproducts from '../resources/Images/variableptsicon.svg'
+import importicon from '../resources/Images/importicon.svg'
+import search from '../resources/Images/search.svg'
+import vendors from '../resources/Images/vendors.svg'
+
+export {add,arrow,attributes,brands,categories,orders,special,variableproducts,importicon,search,vendors}
