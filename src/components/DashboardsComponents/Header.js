dbe331e7385d66c3e4cead3c31e842fde577198c
frankopkusianwar <okirorfrank3@gmail.com>
2022-07-21 17:32:57 +0300

    Make header a wrapper

--- src/components/DashboardsComponents/Header.js
+++ src/components/DashboardsComponents/Header.js
@@ -2,8 +2,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import useStyles from "./AllStyles";
 
-const Header = () => {
+const Header = ({children}) => {
+
+    const classes = useStyles()
 
     const location = useLocation();
     let navigate = useNavigate();
@@ -13,25 +16,28 @@ const Header = () => {
 
     return(
         <div>
-            <Breadcrumbs aria-label="breadcrumb">
-                {/* <Link color="inherit" onClick={() => navigate('/dashboard')}>
-                    Home
-                </Link> */}
-                {
-                    pathNames.map((name,index) => {
-                        const routeTo = `${pathNames.slice(0, index + 1).join('/')}`
-                        const isLast = index === pathNames.length - 1
-                        return(
-                            isLast ?
-                            <Typography>{name}</Typography>
-                            :
-                            <Link color="inherit" onClick={() => navigate(routeTo)}>
-                               {name}
-                            </Link> 
-                        )
-                    })
-                }
-            </Breadcrumbs>
+            <div className={classes.topNavBar}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    {/* <Link color="inherit" onClick={() => navigate('/dashboard')}>
+                        Home
+                    </Link> */}
+                    {
+                        pathNames.map((name,index) => {
+                            const routeTo = `${pathNames.slice(0, index + 1).join('/')}`
+                            const isLast = index === pathNames.length - 1
+                            return(
+                                isLast ?
+                                <Typography>{name}</Typography>
+                                :
+                                <Link color="inherit" onClick={() => navigate(routeTo)}>
+                                {name}
+                                </Link> 
+                            )
+                        })
+                    }
+                </Breadcrumbs>
+            </div>
+            <div>{children}</div>
         </div>
     )
 }
