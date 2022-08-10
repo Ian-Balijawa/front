0b92b4fbc70c31ec24b142e4a98ceaf750bdfc52
Opel <opegude.n3t@gmail.com>
2022-08-10 09:16:57 +0300

    card final

--- src/components/GPCard/GPCard.js
+++ src/components/GPCard/GPCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 
-const GPCard = ({children}) => {
+const GPCard = ({children,className}) => {
   return (
-      <Card>  
+
+      <Card
+        style={styles.card}
+        className={className}
+      >  
         <Card.Body>
             {children}
         </Card.Body>
@@ -12,3 +16,15 @@ const GPCard = ({children}) => {
 }
 
 export default GPCard
+
+const styles = {
+   card:{
+     backgroundColor:"#FFFEFE",
+     border:"0.5px solid #CBCBCB",
+     borderRadius:"6px",
+     boxShadow:"0px 6px 15px 3px rgba(84, 84, 84, 0.1)",
+     width:"100%"
+
+
+   }
+}
